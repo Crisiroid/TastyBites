5a814324184ae0ed9af64ca13a76616e68c69910
Amir sajjad Hosein Pour <85690802+Crisiroid@users.noreply.github.com>
2025-03-15 23:18:07 +0330

    Created new Controller

---
+++ Recipe_Management/Controllers/FoodController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Recipe_Management.Controllers
+{
+    public class FoodController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
