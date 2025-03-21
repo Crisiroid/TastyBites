71bd461daa18a9b41f7fe917321b96f99f5b6558
Amir sajjad Hosein Pour <85690802+Crisiroid@users.noreply.github.com>
2025-03-22 00:29:01 +0330

    Finished Food recipe controller

--- Recipe_Management/Controllers/FoodController.cs
+++ Recipe_Management/Controllers/FoodController.cs
@@ -28,6 +28,13 @@ namespace Recipe_Management.Controllers
             }
 
         }
+
+        [HttpGet("GetAllFoods")]
+        public IActionResult GetAll()
+        {
+            return Ok(_foodService.GetFoods());
+        }
+
         [HttpPost("CreateFood")]
         public IActionResult Create(Food food)
         {
@@ -37,6 +44,34 @@ namespace Recipe_Management.Controllers
             {
                 _foodService.CreateFood(food);
                 return Ok("Recipe Added");
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch("UpdateFood")]
+        public IActionResult Update(Food food)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Model is not valid");
+            try
+            {
+                _foodService.UpdateFood(food);
+                return Ok("Updated");
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("DeleteFood")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _foodService.DeleteFood(id);
+                return Ok("Deleted");
             }catch(Exception ex)
             {
                 return BadRequest(ex.Message);
