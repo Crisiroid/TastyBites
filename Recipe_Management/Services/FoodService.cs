7b7c5a09ef223403b1f49242d3786f98cc3641d2
Amir sajjad Hosein Pour <85690802+Crisiroid@users.noreply.github.com>
2025-03-22 00:11:29 +0330

    Created Service for food

---
+++ Recipe_Management/Services/FoodService.cs
@@ -0,0 +1,40 @@
+﻿using Recipe_Management.Model;
+using Recipe_Management.Repositories;
+
+namespace Recipe_Management.Services
+{
+    public class FoodService : IFoodService
+    {
+        private readonly IFoodRepository _foodRepository;
+
+        public FoodService(IFoodRepository foodRepository)
+        {
+            _foodRepository = foodRepository;
+        }
+
+        public void CreateFood(Food food)
+        {
+            _foodRepository.Add(food);
+        }
+
+        public void DeleteFood(int id)
+        {
+            _foodRepository.Delete(id);
+        }
+
+        public Food GetFood(int id)
+        {
+            return _foodRepository.GetById(id);
+        }
+
+        public IEnumerable<Food> GetFoods()
+        {
+            return _foodRepository.GetAll();
+        }
+
+        public void UpdateFood(Food food)
+        {
+            return _foodRepository.Update(food);
+        }
+    }
+}
---
+++ Recipe_Management/Services/IFoodService.cs
@@ -0,0 +1,13 @@
+﻿using Recipe_Management.Model;
+
+namespace Recipe_Management.Services
+{
+    public interface IFoodService
+    {
+        IEnumerable<Food> GetFoods();
+        Food GetFood(int id);
+        void CreateFood (Food food);
+        void UpdateFood (Food food);
+        void DeleteFood (int id);
+    }
+}
