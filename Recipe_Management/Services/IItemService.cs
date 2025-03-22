5066c3818d98e6b6dc1bdd3b6ea284e9b2c8cb72
Amir sajjad Hosein Pour <85690802+Crisiroid@users.noreply.github.com>
2025-03-22 10:55:49 +0330

    Created Service for Items

---
+++ Recipe_Management/Services/IItemService.cs
@@ -0,0 +1,13 @@
+﻿using Recipe_Management.Model;
+
+namespace Recipe_Management.Services
+{
+    public interface IitemService
+    {
+        IEnumerable<Item> GetItems();
+        Item GetItem(int Id);
+        void CreateItem(Item item);
+        void DeleteItem(int Id);
+        void UpdateItem(Item item);
+    }
+}
---
+++ Recipe_Management/Services/ItemService.cs
@@ -0,0 +1,57 @@
+﻿using Recipe_Management.Model;
+using Recipe_Management.Repositories;
+
+namespace Recipe_Management.Services
+{
+    public class ItemService : IitemService
+    {
+        private readonly IItemRepository _itemRepository;
+        public ItemService(IItemRepository itemRepository)
+        {
+            _itemRepository = itemRepository;
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            return _itemRepository.GetAll();
+        }
+        public Item GetItem(int Id)
+        {
+            return _itemRepository.GetById(Id);
+        }
+
+        public void CreateItem(Item item)
+        {
+            try
+            {
+                _itemRepository.Add(item);
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void DeleteItem(int Id)
+        {
+            try
+            {
+                _itemRepository.Delete(Id);
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public void UpdateItem(Item item)
+        {
+            try
+            {
+                _itemRepository.Update(item);
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
