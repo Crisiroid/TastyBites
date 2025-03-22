912ab3d856dc30d9e544c269255fe16383440216
Amir sajjad Hosein Pour <85690802+Crisiroid@users.noreply.github.com>
2025-03-22 11:04:38 +0330

    Finished Controller for Items

---
+++ Recipe_Management/Controllers/ItemController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipe_Management.Model;
+using Recipe_Management.Services;
+
+namespace Recipe_Management.Controllers
+{
+    [ApiController]
+    [Route("/v1/[controller]")]
+    public class ItemController : Controller
+    {
+        private readonly IitemService _itemService;
+        public ItemController(IitemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        [HttpGet("GetItem")]
+        public IActionResult GetItem(int id)
+        {
+            return Ok(_itemService.GetItem(id));
+        }
+
+        [HttpGet("GetAllItems")]
+        public IActionResult GetAllItems()
+        {
+            return Ok(_itemService.GetItems());
+        }
+
+        [HttpPost("CreateItem")]
+        public IActionResult Create(Item item)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Model is invalid");
+
+            try
+            {
+                _itemService.CreateItem(item);
+                return Ok("Item Created");
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("DeleteItem")]
+        public IActionResult Delete(int Id)
+        {
+            try
+            {
+                _itemService.DeleteItem(Id);
+                return Ok("Item Removed");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("UpdateItem")]
+        public IActionResult Update(Item item)
+        {
+            try
+            {
+                _itemService.UpdateItem(item);
+                return Ok("Item Updated");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
